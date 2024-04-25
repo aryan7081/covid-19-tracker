8961c18151714a46c6fb9531dbabdccdc53163f9
aryan7081 <haryan458@gmail.com>
2024-04-25 20:49:03 +0530

    build

---
+++ build/precache-manifest.8c8a2edfe553d4454d3d7330566aebc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77ca5d37ea8a1842129e913d4cb409a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d12def9cbffbb1207f6e",
+    "url": "/static/css/2.cee453c7.chunk.css"
+  },
+  {
+    "revision": "dd60ed6669dbb219acc0",
+    "url": "/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "d12def9cbffbb1207f6e",
+    "url": "/static/js/2.790680bb.chunk.js"
+  },
+  {
+    "revision": "8f3c00fcc8696eaf48fe80742cdb06a2",
+    "url": "/static/js/2.790680bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd60ed6669dbb219acc0",
+    "url": "/static/js/main.3c91ecf0.chunk.js"
+  },
+  {
+    "revision": "dd7b6dd6b48561cca9ab",
+    "url": "/static/js/runtime-main.8c9d7948.js"
+  }
+]);
