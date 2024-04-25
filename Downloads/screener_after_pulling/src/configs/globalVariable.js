1b980633dce53581e9308455b41f972473163773
Gangadhar Yadav <haryan458@gmail.com>
2024-04-25 18:55:48 +0530

    Delete Downloads/screener_after_pulling directory

--- Downloads/screener_after_pulling/src/configs/globalVariable.js
+++
@@ -1,5 +0,0 @@
-export const ENV_PROXY=import.meta.env.VITE_PROXY_PROTOCOL + import.meta.env.VITE_PROXY_URL || "";
-export const ENV_CHAT_PROXY=import.meta.env.VITE_PROXY_URL || "";
-export const BACKEND_URL=ENV_PROXY
-export const BACKEND_CHAT_URL=ENV_CHAT_PROXY
-export const MIXPANEL_PROJECT_TOKEN = import.meta.env.VITE_MIXPANEL_PROJECT_TOKEN;
--- Downloads/screener_after_pulling/src/pages/auth/index.jsx
+++
@@ -1,4 +0,0 @@
-export * from "@/pages/auth/authUI";
-export * from "@/pages/auth/signIn";
-export * from "@/pages/auth/beta";
-export * from "@/pages/auth/signUp";
